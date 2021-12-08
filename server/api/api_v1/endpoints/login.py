b7d6ebcf78fdd0b20d656a3ca01be2f987c080bc
René Dohmen <acidjunk@gmail.com>
2021-12-08 02:02:28 +0100

    Removed debug stuff

--- server/api/api_v1/endpoints/login.py
+++ server/api/api_v1/endpoints/login.py
@@ -29,7 +29,6 @@ def login_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     """
     OAuth2 compatible token login, get an access token for future requests
     """
-    logger.info("Form:", form_data=form_data)
     user = user_crud.authenticate(username=form_data.username, password=form_data.password)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
