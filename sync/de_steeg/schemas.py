c412954565e94ff342261c54a8fdee760b593670
Rene Dohmen <acidjunk@gmail.com>
2024-07-17 22:27:48 +0200

    refactor some location and fix build

--- sync/de_steeg/schemas.py
+++ sync/de_steeg/schemas.py
@@ -26,15 +26,3 @@ class Product(BaseModel):
     product_id: int
     name: str
     amount: float
-
-
-class JsonPriceField(BaseModel):
-    """the fields that can be used in the dynamic priceFields columns.
-
-    Note: this is only used by the parser for now. Storing it to the DB as a list of dictionaries.
-    """
-
-    price: float
-    label: str
-    quantity: float
-    active: bool
